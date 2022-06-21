9d6b9e8d9960430c8d021a23fce42689eea6ecdb
Lloyd Theophilus <lloydtheophilus@gmail.com>
2022-06-21 17:21:21 +0100

    Created a post schema

---
+++ models/post.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+const postSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+        maxlength: 20
+    },
+    content: {
+        type: String,
+        required: true,
+        maxlength: 100
+    },
+    img: {
+        type: String
+    },
+    userId: {
+        type: String,
+        required: true
+    },
+    likes: {
+        type: Array,
+        default: []
+    },
+    comments: {
+        type: Array,
+        default: []
+    }
+},
+    { timestamps: true }
+);
+
+module.exports = mongoose.model('Post', postSchema);
